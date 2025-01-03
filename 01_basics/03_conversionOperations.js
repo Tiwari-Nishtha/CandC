4fbf820bf1748f26bac54278a1c6a14fd76de25b
Tiwari-Nishtha <nishtha.code@gmail.com>
2025-01-03 16:55:11 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,47 @@
+
+/*
+let score=33
+let marks="33abc"
+let n=null
+let u=undefined
+
+console.log(typeof(score))
+console.log(typeof(marks))
+
+let valueInNumber=Number(marks)
+console.log(typeof(marks))
+console.log(valueInNumber)  //NaN= not a number
+
+let vin=Number(n)
+console.log(typeof(n))  //object 
+console.log(vin)    //0
+
+let valueInNum=Number(u)
+console.log(typeof(u))   //undefined
+console.log(valueInNum)    //NaN
+
+
+/*
+"33" => 33
+"33abc" =>NaN
+true=>1
+false =>0
+*/
+
+/*
+let isLoggedIn=1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+
+1=>true
+0=>false
+"" => false
+"anything" =>true
+*/
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+
